dd50d418dc6484eb77d96d068a9288a149628720
dltbwoddl <60048456+dltbwoddl@users.noreply.github.com>
2020-06-16 17:45:26 +0900

    Add files via upload

---
+++ if 문/if문 풀이 알람시계.py
@@ -0,0 +1,26 @@
+
+#시간:64ms 코드길이:249B
+a=[int(i) for i in input('').split()]
+if a[0]==0:
+    if a[1]<=44:
+        a[1]=60 - (45-a[1])
+        a[0]=23
+    else:
+        a[1]-=45
+else:
+    if a[1]<=44:
+        a[1]=60 - (45-a[1])
+        a[0]-=1
+    else:
+        a[1]-=45
+print(a[0],a[1])
+
+#시간:68ms 코드길이:136B
+a=[int(i) for i in input('').split()]
+a[1]=a[1]-45
+if a[1]<0:
+    a[0]-=1
+    a[1]+=60 
+    if a[0]<0:
+        a[0]=23
+print(a[0],a[1])
---
+++ 입출력과 사칙연산/a나누기b.py
@@ -0,0 +1,2 @@
+a=input("").split()
+print(eval(a[0]+'/'+a[1]))
