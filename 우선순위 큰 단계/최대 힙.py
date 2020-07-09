d99f27c0e5f79c671f0c4bb1b6363dbfcb726608
dltbwoddl <60048456+dltbwoddl@users.noreply.github.com>
2020-07-09 09:52:17 +0900

    Add files via upload

---
+++ 우선순위 큰 단계/최대 힙.py
@@ -0,0 +1,17 @@
+#시간초과
+#remove는 o(n)   
+#https://www.acmicpc.net/problem/11279
+n=int(input(""))
+result=[]
+alist=[]
+for i in range(0,n):
+    n_1=int(input(""))
+    alist.append(n_1)
+    if n_1==0:
+        if result==[]:
+            result.append(0)
+        else:
+            result.append(max(alist))
+            alist.remove(max(alist))
+for i in result:
+    print(i)
